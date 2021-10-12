5cc3996454b3b1a83a9c3d114e16540569e010b8
Wend Carlos <wendcarlos18@hotmail.com>
2021-10-12 17:14:09 -0300

    create global styles in react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './globalStyle';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// No react os componentes são como sanduiche, um tem que está dentro do outro
+ReactDOM.render(
+    <>
+        <App /> <GlobalStyle/>
+    </>, 
+    document.getElementById('root')
+);
 
 
